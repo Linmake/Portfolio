fee1e80b50d832719e4facfed6d8c03b175e75dc
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-07-17 01:40:04 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <h1 className='logo'>Nicole Figueroa</h1>
+        <h2>Diseñadora web y programadora</h2>
+        <p className="read-the-docs">
+          Portafolio en mantenimiento
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
