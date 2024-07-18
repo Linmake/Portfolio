7011e37b8cfde284031aaee0123dd64eb4b1e3fe
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-07-18 07:43:16 -0700

    icon & fonts render

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className='container'>
         <h1 className='logo'>Nicole Figueroa</h1>
-        <h2>Diseñadora web y programadora</h2>
+        <h2>Diseñadora web & programadora</h2>
         <p className="read-the-docs">
           Portafolio en mantenimiento
         </p>
