d7798f7fadbe53e6552a58b860b5c321fa4f6778
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-07-18 17:00:18 -0700

    built de icono de imagen responsive actualizado

---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+import "../style/loading"
+export default function Loader() {
+  return (
+    <>
+      <span className="loader"></span>
+    </>
+  )
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { copy } from 'vite-plugin-copy';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: 'public/ICONS/*', dest: 'dist/ICONS' }
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+});
