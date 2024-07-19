6895aa30e4dcef5622970d57b51fbd355b3d22a9
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-07-18 18:38:49 -0700

    s

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { copy } from 'vite-plugin-copy';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    copy({
+    viteStaticCopy({
       targets: [
-        { src: 'public/ICONS/*', dest: 'dist/ICONS' }
+        {
+          src: 'public/ICONS/*',
+          dest: 'ICONS'
+        }
       ]
     })
   ],
