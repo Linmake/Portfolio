77c06894561cc255c269b5f8cfe3385ee49cbe3a
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-07-19 16:43:24 -0700

    actualizacion del nuevo icono de sitio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,7 @@ export default function Header() {
   return (
     <>
       <header>
-        <h1 className="titulo">Nicole Figueroa</h1>
-        <h2 className="subtitulo">Diseñadora web & programadora.</h2>
+
       </header>
     </>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,8 @@ import '../style/Main.css'
 export default function Main() {
   return (
     <main>
+      <h1 className="titulo">Nicole Figueroa</h1>
+      <h2 className="subtitulo">Diseñadora web & programadora.</h2>
       <p className="read-the-docs">
         Portafolio en mantenimiento.
       </p>
