42d190430b2ea1b60e537abae604d5a4f8209235
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-07-20 18:14:44 -0700

    actualizar assetsDir

--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html')
