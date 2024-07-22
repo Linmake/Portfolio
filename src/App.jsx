05d64f9cecc87b13e7406f121e04b4ab1e2d49be
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-07-21 19:54:05 -0700

    elimanado test de parallax en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
+
 function App() {
   return (
-    <>
-      <div className='container'>
-        <Header />
-        <Main />
-      </div>
-    </>
+    <div className='container'>
+      <Header />
+      <Main />
+    </div>
   )
 }
+
 export default App
