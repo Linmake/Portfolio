09ec1e9b1815029586ccb919f57beb9efaae376b
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-07-21 19:50:58 -0700

    agregacion de parallax en main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,21 @@
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import '../style/Main.css'
 
 export default function Main() {
   return (
     <main>
       <div className="title-content">
-        <h1 className="titule">Nicole Figueroa</h1>
-        <h2 className="subtitle">Diseñadora web & programadora.</h2>
+        <Parallax pages={2} className="animation" style={{ top: '11%', left: '0' }}>
+          <ParallaxLayer offset={0} speed={0.2} >
+            <div className="animation_layer parallax">
+              <h1 className="title">Nicole Figueroa</h1>
+              <h2 className="subtitle">Diseñadora web & programadora.</h2>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={0.2}>
+            <div className="animation_layer parallax" id="star"></div>
+          </ParallaxLayer>
+        </Parallax >
       </div>
     </main>
   )
