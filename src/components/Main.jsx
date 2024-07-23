7c9b59e8a41abfe5555e50d908587d73cf18e75a
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-07-23 13:59:17 -0700

    creacion de maquetacion de about me y proyects

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,7 +12,7 @@ export default function Main() {
   const styles = useSpring({
     transform: inView
       ? 'scale(1)'
-      : 'scale(0.93);',
+      : 'scale(0.93)',
     opacity: inView ? 1 : 0.35,
     config: { duration: 1500 },
   });
@@ -34,20 +34,40 @@ export default function Main() {
                   </div>
                 </div>
                 <div id="portImageCont">
-                  <img id="astro" src="../../public/assets/astro.webp" alt="" />
+                  <img id="astro" src="../../public/assets/header-img.svg" alt="" />
                 </div>
               </div>
             </div>
           </ParallaxLayer>
           <ParallaxLayer offset={1} speed={0.2}>
             <div id="content-2">
-              <animated.img
-                ref={ref}
-                src="/assets/foto.png"
-                alt="img of desert"
-                className="img"
-                style={styles}
-              />
+              <div id="about-content">
+                <h1>About Me</h1>
+                <p></p>
+                <animated.img
+                  ref={ref}
+                  src="/assets/me.jpg"
+                  alt="img of desert"
+                  className="img"
+                  style={styles}
+                />
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1} speed={0.2}>
+            <div id="content-3">
+              <div className="card-proyect-content">
+                <img></img>
+                <div className="info-card">
+                  <div className="tecnology-content">
+                    <div className="tecnology" id="tec1">React</div>
+                    <div className="tecnology" id="tec1">Node</div>
+                    <div className="tecnology" id="tec1">Mongo</div>
+                  </div>
+                  <h1>Notes</h1>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptas eligendi animi. Corporis, facere ipsum dolor sit amet consectetur, adipisicing elit. Consectetur quae aperiam obcaecati?</p>
+                </div>
+              </div>
             </div>
           </ParallaxLayer>
         </Parallax>
