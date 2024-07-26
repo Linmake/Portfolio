5c46d9dc01453930242f7833d14d7e0faeee2335
Linda Nicole <96255722+Linmake@users.noreply.github.com>
2024-07-25 22:41:13 -0700

    fix preload font #15

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -9,31 +9,31 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   server: {
-    host: true
+    host: true,
   },
   plugins: [
     react(),
-    // viteStaticCopy({
-    //   targets: [
-    //     {
-    //       src: 'public/icons/*',
-    //       dest: 'icons'
-    //     }
-    //   ]
-    // })
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/icons/*',
+          dest: 'icons', // Esto coloca los archivos en la carpeta "icons" dentro de la salida de construcción
+        },
+      ],
+    }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html')
-      }
-    }
-  }
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 });
